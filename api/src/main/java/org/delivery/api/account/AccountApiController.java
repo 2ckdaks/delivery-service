3a89de876accd0283283e4355d330afd5acbc0d5
이창민 <2ckdaks@gmail.com>
2024-05-21 13:20:32 +0900

    Exception Handler 적용 (예외처리)

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,7 +3,9 @@ package org.delivery.api.account;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.ErrorCode;
 import org.delivery.api.common.error.UserErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,13 +22,23 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public Api<Object> me(){
+    public Api<AccountMeResponse> me(){
+
         var response = AccountMeResponse.builder()
                 .name("이창민")
                 .email("2ckdaks@gmail.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
 
-        return Api.ERROR(UserErrorCode.USER_NOT_FOUND, "이창민 이라는 사용자 없음");
+        var str = "테스트";
+        var age = 0;
+
+        try{
+            Integer.parseInt(str);
+        }catch (Exception e){
+            throw new ApiException(ErrorCode.SERVER_ERROR, e, "사용자 Me 호출시 에러 발생");
+        }
+
+        return Api.OK(response);
     }
 }
