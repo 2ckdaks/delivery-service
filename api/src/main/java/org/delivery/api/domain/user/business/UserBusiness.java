8d6d6d42d202709acc26885c8a34694630d0b054
이창민 <2ckdaks@gmail.com>
2024-05-22 16:32:04 +0900

    사용자 로그인 로직 처리(토큰 생성 전)

--- api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -2,14 +2,11 @@ package org.delivery.api.domain.user.business;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
-import org.delivery.api.common.error.ErrorCode;
-import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
 import org.delivery.api.domain.user.service.UserService;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Business
@@ -31,5 +28,19 @@ public class UserBusiness {
         var newEntity = userService.register(entity);
         var response = userConverter.toResponse(newEntity);
         return response;
+    }
+
+    /**
+     * 1. email, password로 사용자 체크
+     * 2. user entity 로그인 확인
+     * 3. token 생성
+     * 4. token response
+     */
+    public UserResponse login(UserLoginRequest request) {
+        var userEntity = userService.login(request.getEmail(), request.getPassword());
+        // 사용자가 없으면 throw
+        
+        // TODO 토큰 생성 로직으로 변경하기
+        return userConverter.toResponse(userEntity);
     }
 }
--- api/src/main/java/org/delivery/api/domain/user/service/UserService.java
+++ api/src/main/java/org/delivery/api/domain/user/service/UserService.java
@@ -1,7 +1,9 @@
 package org.delivery.api.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.api.Api;
 import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.user.UserEntity;
 import org.delivery.db.user.UserRepository;
@@ -29,5 +31,24 @@ public class UserService {
                     return userRepository.save(userEntity);
                 })
                 .orElseThrow(()-> new ApiException(ErrorCode.NULL_POINT, "User Entity Null"));
+    }
+
+    public UserEntity login(
+            String email,
+            String password
+    ){
+        var entity = getUserWithThorw(email, password);
+        return entity;
+    }
+
+    public UserEntity getUserWithThorw(
+            String email,
+            String password
+    ){
+        return userRepository.findFirstByEmailAndPasswordAndStatusOrderByIdDesc(
+                email,
+                password,
+                UserStatus.REGISTRED
+        ).orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
     }
 }
